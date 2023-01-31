bae3481a645c70d843f00cf4b24269ff3c563d51
Tim Rakh <nbveh0914@gmail.com>
2023-01-31 14:37:11 +0300

    Commit

---
+++ src/Worker.java
@@ -0,0 +1,19 @@
+public class Worker  {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallBack;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallBack) {
+        this.callback = callback;
+        this.errorCallBack = errorCallBack;
+    }
+
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33){
+                errorCallBack.onError("Task " + i + " is error");
+            }else callback.onDone("Task " + i + " is done");
+        }
+    }
+
+}
